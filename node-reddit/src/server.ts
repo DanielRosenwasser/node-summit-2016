532df500d462e3ea561710003064e3ee336aeadd
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2016-07-26 22:32:43 -0700

    Allow an offline mode.

--- node-reddit/src/server.ts
+++ node-reddit/src/server.ts
@@ -1,22 +1,31 @@
 import * as express from "express";
 
-import { httpRequest } from "./promisified-io";
+import { httpRequest, readFile } from "./promisified-io";
 import * as reddit from "./reddit";
 
+const staticDir = __dirname + "/../../react-reddit";
+
 async function getRedditFeed(subreddit: string) {
-    let resultStr = await httpRequest(`http://reddit.com/r/${subreddit}.json`);
-    let result = JSON.parse(resultStr) as reddit.ApiResponse;
+    let resultJsonStr = await getRedditJson(subreddit, /*offline*/ false)
+    let result = JSON.parse(resultJsonStr) as reddit.ApiResponse;
 
     // Only return submissions that link to jpgs and pngs.
     result.data.children = result.data.children.filter(sub => sub.data.url.match(/(jpg|png)$/));
     return result;
 }
 
+function getRedditJson(subreddit: string, offline: boolean) {
+    if (offline) {
+        return readFile(`${staticDir}/data/${subreddit}.json`);
+    }
+    return httpRequest(`http://reddit.com/r/${subreddit}.json`);
+}
+
 const app = express();
 
 app.get("/r/:subreddit.json", async (req, res) => {
     try {
-        let body = await getRedditFeed(req.params.subreddit)
+        let body = await getRedditFeed(req.params.subreddit);
         res.setHeader("Content-Type", "application/json");
         res.send(body);
     }
@@ -25,7 +34,7 @@ app.get("/r/:subreddit.json", async (req, res) => {
     }
 });
 
-app.use("/", express.static(__dirname + "/../../react-reddit"));
+app.use("/", express.static(staticDir));
 
 const server = app.listen(8000, () => {
     console.log("Server listening on port 8000");
